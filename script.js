13c1fad537adf88316f35c876e2d708e649fc389
AndyMagwayer <andymagwayer@gmial.com>
2023-08-18 13:07:24 +0500

    feat: fixed merging

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let answer = +prompt('Введите цифру недели и узнайте её перевод на Английском')
+let answer = +prompt('Введите ваш возраст и я скажу ваш стаж работы')
 
-if (answer === 1  ){
-    alert('Monday')
+if (answer === 0  ){
+    alert('Вам ещё рости и рости')
 }
-else if(answer === 2) {
-  alert('Tuesday')
+else if(answer > 150) {
+  alert('You will die man hahahahha')
 }
-else if(answer ===3) {
-  alert('Wedneday')
+else if(answer > 0 && answer <= 8) {
+  alert('Вам ещё рости и рости')
 }
 
-else if(answer === 4) {
-    alert('Thursday')
+else if(answer >= 9 && answer <= 20) {
+    alert('Вам ещё учиться и учиться')
   }
 
-  else if(answer === 5) {
-    alert('Friday')
+  else if(answer >= 21 && answer <= 65) {
+    alert('Вам ещё работать и работать')
   }
 
-  else if(answer === 6) {
-    alert('Saturday')
+  else if(answer >= 66 && answer <= 99) {
+    alert('Вам уже на пенсию надо')
   }
 
-  else if(answer ===7) {
-    alert('Sunday')
+  else if(answer >= 100) {
+    alert('YOu will DIe')
   }
 
   else {
